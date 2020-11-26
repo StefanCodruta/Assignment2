161bfee35c15b1b68887f1ad8ebb568617f41f6e
Stefan Codruta <stefancodruta18@gmail.com>
2020-11-26 20:44:11 +0200

    done

--- main/app.js
+++ main/app.js
@@ -1,5 +1,30 @@
 function addTokens(input, tokens){
+    if(typeof input!=="string"){
+        throw ("Invalid input");
+    }
+    if(input.length<6){
+        throw ("Input should have at least 6 characters");
+
+    }
+
     
+
+    for(i=0;i<tokens.length;i++){
+
+        if(typeof tokens[i].tokenName!=="string"){
+            throw ("Invalid array format");
+        }
+    }
+    
+    
+    if(input.includes("..."))
+    {
+        for(i=0;i<tokens.length;i++){
+            input = input.replace("...", "${"+tokens[i].tokenName+"}");
+            return input;
+        }
+    }
+    else return input;
 }
 
 const app = {
